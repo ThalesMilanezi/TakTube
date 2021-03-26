254756b24566f1dbc09ee86c16153cb8c344a0da
Thales Milanezi <milanezitf@gmail.com>
2021-03-26 16:11:29 -0300

    fix: remove quotes from properties on ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,16 @@
 export default {
-  "type": process.env.TYPEORM_CONNECTION,
-  "host": process.env.TYPEORM_HOST,
-  "port": process.env.TYPEORM_PORT,
-  "username": process.env.TYPEORM_USERNAME,
-  "password": process.env.TYPEORM_PASSWORD,
-  "database": process.env.TYPEORM_DATABASE,
-  "synchronize": process.env.TYPEORM_SYNCHRONIZE,
-  "logging": process.env.TYPEORM_LOGGING,
-  "entities": [ 
-    "src/data/entity/**/*.ts" ], 
- "migrations": [ "src/data/migration/**/*.ts" 
- ], 
- "cli": { 
-    "entitiesDir":"src/data/entity", "migrationsDir":"src/data/migration"
- }    
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE,
+  logging: process.env.TYPEORM_LOGGING,
+  entities: ['src/data/entity/**/*.ts'],
+  migrations: ['src/data/migration/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/data/entity',
+    migrationsDir: 'src/data/migration',
+  },
 }
